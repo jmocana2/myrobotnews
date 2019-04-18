c3c5a61fc1e28d3453445b41f1a81c3eb6d9ab31
jmocana <telecomu4@gmail.com>
2019-04-18 11:56:15 +0200

    added Material-UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
@@ -10,7 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
-          </p>
+          </p>          
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -19,6 +20,9 @@ class App extends Component {
           >
             Learn React
           </a>
+          <Button variant="contained" color="primary">
+            Hello World from material-UI
+          </Button>
         </header>
       </div>
     );
