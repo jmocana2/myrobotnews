7c17a033eee95f4057f8ad2dcbd8d29c0ee94935
jmocana <telecomu4@gmail.com>
2019-04-18 20:25:11 +0200

    added styled components and configured to work with material UI. Using Babel macro for use css attribute of styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import JssProvider from "react-jss/lib/JssProvider";
+import { create } from "jss";
+import styled from 'styled-components/macro'
+import { createGenerateClassName, jssPreset } from "@material-ui/core/styles";
+import Button from '@material-ui/core/Button';
 import logo from './logo.svg';
 import './App.css';
-import Button from '@material-ui/core/Button';
+
+const StyledButton = styled(Button)`
+  background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
+  border-radius: 3px;
+  border: 0;
+  color: white;
+  height: 48px;
+  padding: 0 30px;
+  box-shadow: 0 3px 5px 2px rgba(255, 105, 135, .3);
+`;
+
+const styleNode = document.createComment("insertion-point-jss");
+document.head.insertBefore(styleNode, document.head.firstChild);
+
+const generateClassName = createGenerateClassName();
+const jss = create({
+  ...jssPreset(),
+  insertionPoint: "insertion-point-jss"
+});
 
 class App extends Component {
   render() {
     return (
+      <JssProvider jss={jss} generateClassName={generateClassName}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -16,15 +40,18 @@ class App extends Component {
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noopener noreferrer" 
+            css={`margin-bottom: 20px`}           
           >
             Learn React
           </a>
-          <Button variant="contained" color="primary">
-            Hello World from material-UI
-          </Button>
+          <StyledButton variant="contained" color="primary">
+            Hello, material-UI with styled components
+          </StyledButton>
+          <p css={`color: red`}>Probando atributo css</p>
         </header>
       </div>
+      </JssProvider>
     );
   }
 }
