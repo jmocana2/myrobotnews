6b3e84aab31951ce6ff8234f0a84c2553311bdbb
jmocana2 <telecomu4@gmail.com>
2019-07-01 09:32:48 +0200

    Styles and story for Date component. added polished.

--- src/components/core/date/stories.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-import { storiesOf } from '@storybook/react';
-
-storiesOf('Fecha', module).add('default', () => <p>Componente fecha</p>);
---
+++ src/components/core/date/view/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StyledDate from './style';
+
+const Date = () => {
+  return (
+    <StyledDate dateTime="2019-01-07 08:19">
+      Lunes, 1 de julio de 2019
+    </StyledDate>
+  );
+};
+
+export default Date;
