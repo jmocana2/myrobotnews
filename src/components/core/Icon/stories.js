3bedd0a4061c1c871cbac8f5fc1b81a8c51b9718
jmocana2 <telecomu4@gmail.com>
2019-07-09 09:01:57 +0200

    Testing illustrartor and svgs

--- src/components/core/Icon/stories.js
+++ src/components/core/Icon/stories.js
@@ -4,12 +4,16 @@ import Icon from '.';
 
 /** Icon library */
 import IconAlert from '../../../../assets/images/svg/alert-icon.svg';
+import IconClock from '../../../../assets/images/svg/reloj-arena.svg';
 
 storiesOf('core/Icons', module).add('Iconos Canales', () => (
-  <div>
+  <div style={{ backgroundColor: '#000' }}>
     <ul>
       <li>
-        <Icon svg={IconAlert} />
+        <Icon svg={IconAlert} fill="white" />
+      </li>
+      <li>
+        <Icon svg={IconClock} />
       </li>
     </ul>
   </div>
